417fccba5c65068ffcf80d41fb39d7c8e5b4c664
Susan Tan <susan.tan198@gmail.com>
2019-01-29 20:05:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,6 +13,7 @@ function whileLoop(n){
   while(n > 0){
     console.log(n--);
   }
+<<<<<<< HEAD
   return "done";
 }
 
@@ -23,3 +24,27 @@ do{
 } while (arr.length > 0);
 return arr;
 }
+=======
+  return "done"
+}
+
+function doWhileLoop(arr){
+do{
+  arr.pop();
+} while (arr.length > 0)
+}
+
+/*
+  describe('doWhileLoop(array)', () => {
+    it('removes elements from `array` until `array` is empty or until `incrementVariable()` returns `false`', () => {
+      const [array, t] = makeArray()
+      const l = array.length
+
+      const newArray = doWhileLoop(array)
+
+      expect(newArray).to.have.length.of.at.most(l - 1)
+    })
+  })
+})
+*/
+>>>>>>> 3c216cad06edcf54b3e118335a490a52171b41e6
